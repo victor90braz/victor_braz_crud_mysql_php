d82635c3419633ebc49523a63da7cb8e81da781e
victor90braz <victor.90braz@gmail.com>
2022-10-18 16:11:01 +0200

    Post data from the form to database mysql

--- database/connect_database.php
+++ database/connect_database.php
@@ -2,8 +2,6 @@
 
 $connexion_to_data_base = mysqli_connect("localhost", "root", "", "crud_mysql_php");
 
-
 if (isset($connexion_to_data_base)) {
-echo "DataBase is connected.";
+    echo "DataBase is connected. <br>";
 }
- 
