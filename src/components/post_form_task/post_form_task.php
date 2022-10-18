b94ea9e189264360266bf54f9e6afea75ea6aff6
victor90braz <victor.90braz@gmail.com>
2022-10-18 13:17:32 +0200

    Added a label to the inputs

--- src/components/post_form_task/post_form_task.php
+++ src/components/post_form_task/post_form_task.php
@@ -4,15 +4,19 @@
 
       <div class="card card-body">
         <form action="/victor_braz_crud_mysql_php/src/components/post_form_task/saved_task_form.php" method="POST">
+
             <div class="form-group">
-              <input type="text" name="title" class="form-control mb-2" placeholder="Task Title" autofocus />
+              <label for="title"></label>
+              <input type="text" id="title" name="title" class="form-control mb-2" placeholder="Task Title" autofocus />
             </div>
 
             <div class="form-group mb-1">
               <textarea name="description" rows="2" class="form-control mb-2" placeholder="Task Description"></textarea>
             </div>
 
-            <input type="submit" class="btn btn-success btn-block" name="save_task" value="Save Task">
+            <label for="save_task"></label>
+            <input type="submit" id="save_task" class="btn btn-success btn-block" name="save_task" value="Save Task">
+
           </form>
       </div>
 
