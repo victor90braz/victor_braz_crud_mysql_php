72be9c3b82d63528322604e81fc259d967ec79a5
victor90braz <victor.90braz@gmail.com>
2022-10-19 18:24:46 +0200

    Added form edit component

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include "./database/connect_database.php"?>
 <?php include "./includes/header/header.php"?>
 <?php include "./src/components/post_form_task/post_form_task.php"?>
-<?php include "./src/components/edit/edit2.php"?>
+<?php include "./src/components/edit/editHtml.php"?>
 <?php include "./includes/footer/footer.php";
--- src/components/edit/edit.php
+++ src/components/edit/edit.php
@@ -10,10 +10,42 @@ if (isset($_GET['id'])) {
         $row = mysqli_fetch_array($result);
         $title = $row['title'];
         $description = $row['description'];
+        $id = $row['id'];
 
         $_SESSION["title"] = $title;
         $_SESSION["description"] = $description;
+        $_SESSION["id"] = $id;
 
         header("Location: http://localhost/victor_braz_crud_mysql_php/index.php");
     }
 }
+;
+
+?>
+
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <div class="card mt-4">
+        <div class="card-body">
+
+          <a href="http://localhost/victor_braz_crud_mysql_php/index.php" class="btn btn-info"><i class="fa-solid fa-angles-left"></i> Back</a>
+          <hr>
+
+          <form action="/victor_braz_crud_mysql_php/src/components/edit/edit.php?id=<?php echo $_GET[$_SESSION['id']]; ?>" method="POST">
+            <label for="title"></label>
+            <input type="text" class="form-control" id="title" name="title" value=<?=$_SESSION['title']?> placeholder="Task Title">
+
+            <textarea type="description" class="form-control"  name="description" rows="2" placeholder="Task Description"> <?=$_SESSION['description']?> </textarea>
+
+            <button class="btn btn-warning mt-3"><i class="fa-regular fa-floppy-disk"></i> Update Register</button>
+          </form>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
