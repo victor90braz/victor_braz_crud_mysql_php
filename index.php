290b20c30fe2bfeb66cee0c8fe57dd6a3378b400
victor90braz <victor.90braz@gmail.com>
2022-10-20 12:01:03 +0200

    Added update component

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include "./database/connect_database.php"?>
 <?php include "./includes/header/header.php"?>
 <?php include "./src/components/post_form_task/post_form_task.php"?>
-<?php include "./src/components/edit/editHtml.php"?>
+<?php include "./src/components/update_form_task/update_form_task.php"?>
 <?php include "./includes/footer/footer.php";
